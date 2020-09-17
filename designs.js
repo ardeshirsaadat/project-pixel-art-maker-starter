8375f0d149971dc720924903a0326d4c6c8cfc7b
ardeshirsaadat <ardeshirsaadat@gmail.com>
2020-09-17 11:53:41 -0400

    Color pixel in grid

--- designs.js
+++ designs.js
@@ -4,7 +4,27 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
-
+    const tableElement = document.getElementById("pixelCanvas");
+    let form = document.querySelector("#sizePicker");
+    form.addEventListener('submit', function(event){
+        event.preventDefault()
+        const heightValue = document.getElementById("inputHeight").value;
+        const widthValue = document.getElementById("inputWidth").value;
+        for (let i = 1;i <= Number(widthValue); i++){
+            var newTrElement = document.createElement("TR");
+            for (let j = 1; j <= Number(heightValue); j++){
+                var newTdElement = document.createElement("TD");
+                newTdElement.classList.add("r"+i+"c"+j);
+                newTrElement.appendChild(newTdElement);
+            };
+            tableElement.appendChild(newTrElement);
+        };
+        
+        })
+    var pickColor = document.querySelector("#colorPicker")
+    tableElement.addEventListener('click',function(event){
+        event.target.style.background = pickColor.value
+    })
 }
+
+makeGrid();
